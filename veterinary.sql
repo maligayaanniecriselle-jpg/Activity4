53bad533ea7eef7df4a8b7ae997c131c4d1058eb
Fadriquelan Noel Andrei L <fadriquelannoel28@gmail.com>
2025-12-01 20:35:42 +0800

    feat/create-table-medicalrecords

--- veterinary.sql
+++ veterinary.sql
@@ -44,3 +44,13 @@ CREATE TABLE invoices (
     totalamount DECIMAL(10,2),
     paymentdate TIME
 )
+
+CREATE TABLE medicalrecords (
+    recordid INT PRIMARY KEY,
+    animalid INT,
+    recorddate DATETIME ,
+    doctorid INT,
+    diagnosis VARCHAR(255),
+    prescription VARCHAR(255),
+    notes TEXT
+)
