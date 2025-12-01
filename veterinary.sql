f2f0022cb419c8e6e4b9e2f157e40d986e220ba3
maligayaanniecriselle@gmail.com <maligayaanniecriselle@gmail.com>
2025-12-01 20:29:26 +0800

    feat/create-table-invoices

--- veterinary.sql
+++ veterinary.sql
@@ -37,3 +37,10 @@ CREATE TABLE doctors (
     phone VARCHAR(15),
     email VARCHAR(100)
 );
+
+CREATE TABLE invoices (
+    invoiceid INT PRIMARY KEY,
+    appointid INT,
+    totalamount DECIMAL(10,2),
+    paymentdate TIME
+)
