a2bbcf18e2f2da8ba05eee4f0fed0de670430a59
maligayaanniecriselle@gmail.com <maligayaanniecriselle@gmail.com>
2025-12-01 20:52:56 +0800

    feat/insert-10-rows-owners

--- veterinary.sql
+++ veterinary.sql
@@ -54,3 +54,16 @@ CREATE TABLE medicalrecords (
     prescription VARCHAR(255),
     notes TEXT
 )
+
+INSERT INTO owners (ownerid, ofirstname, olastname, address, phone, email)
+VALUES
+(1, 'Annie', 'Maligaya', 'Antipolo Del Sur', '0911-111-1111', 'anniemaligaya@gmail.com'),
+(2, 'Joshua', 'Castronuevo', 'Baybayin', '0922-222-2222', 'joshuacas@gmail.com'),
+(3, 'Cathrine', 'Balmes', 'Pinagkawitan', '0933-333-3333', 'catbalmes@gmail.com'),
+(4, 'Theresa', 'Atienza', 'Pinagkawitan', '0944-444-4444', 'anniemaligaya@gmail.com'),
+(5, 'Noel', 'Fadriquelan', 'Pinagkawitan', '0955-555-5555', 'noelfadriquelan@gmail.com'),
+(6, 'Kaith', 'Atienza', 'Pinagkawitan', '0966-666-6666', 'kit@gmail.com'),
+(7, 'John', 'Doe', 'US', '0977-777-7777', 'johndoe@gmail.com'),
+(8, 'Lerum', 'Epsum', 'US', '0988-888-8888', 'loremepsum@gmail.com'),
+(9, 'Satao', 'Kid', 'Japan', '0999-999-9999', 'sataokid@gmail.com'),
+(10, 'Ching', 'Choa', 'China', '0900-000-0000', 'chinise@gmail.com')
